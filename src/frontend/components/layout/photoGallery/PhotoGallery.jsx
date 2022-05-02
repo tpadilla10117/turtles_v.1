5493c0c3e12a0a90cedbfc8e1269b9d67b8d288e
Trinidad Padilla <tpadilla101@live.com>
2022-05-02 16:40:45 -0700

    debugging click function

--- src/frontend/components/layout/photoGallery/PhotoGallery.jsx
+++ src/frontend/components/layout/photoGallery/PhotoGallery.jsx
@@ -3,6 +3,38 @@ import React, { useState, useCallback } from 'react';
 import Carousel, { Modal, ModalGateway } from "react-images"; */
 
 function PhotoGallery( { photos } ) {
+
+    let slideIndex = 1;
+
+    const openModal = () => {
+        console.log('clicked')
+        /* document.getElementById('gallery-modal-parent').style.display = 'block'; */
+    };
+
+    const closeModal = () => {
+        document.getElementById('gallery-modal-parent').style.display = 'none';
+    };
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("modal-myslides");
+        /* let dots = document.getElementsByClassName("demo"); */
+        /* var captionText = document.getElementById("caption"); */
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        /* for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        } */
+        slides[slideIndex-1].style.display = "block";
+        /* dots[slideIndex-1].className += " active";
+        captionText.innerHTML = dots[slideIndex-1].alt; */
+    };
+
+
+
 /* State for current viewed photo in gallery: */
     /* const [currentImage, setCurrentImage] = useState(0);
     const [viewerIsOpen, setViewerIsOpen] = useState(false);
@@ -61,18 +93,42 @@ function PhotoGallery( { photos } ) {
             </div>
 
             <div id='gallery-modal-parent' className='gallery-modal-parent'>
-                <span className='modal-close cursor'>& times</span>
+                <span className='modal-close cursor'
+                    onClick={closeModal}
+                >
+                    & times
+                </span>
                 <div className='gallery-modal-content'>
                     {photos.map( mySlides => {
                         return (
-                            <div className='modal-myslides'>
-                                <div className='modal-numbertext'>
+                            <div className='modal-myslides' key={mySlides.id}>
+                                <div className='modal-numbertext'
+                                >
                                     {mySlides.id} / 4
                                 </div>
-                                <img src={mySlides.src} />
+                                <img onClick={openModal} src={mySlides.src} alt={mySlides.id} />
                             </div>
                         )
                     })}
+
+                {/* TODO: Next/Prev controls: */}
+
+                    <a className="modal-prev" /* onClick="plusSlides(-1)" */>&#10094;</a>
+                    <a className="modal-next" /* onClick="plusSlides(1)" */>&#10095;</a>
+
+
+                {/* TODO: Caption Text */}
+                    <div className='gallery-caption-parent-container'>
+                        {photos.map( captions => {
+                            return (
+                                <p key={captions.id} id='caption'></p>
+                            )
+                        })}
+
+                    </div>
+
+                {/* TODO: Thumbnaul Img controls: */}
+
 
                 </div>
 
--- src/frontend/seed.js
+++ src/frontend/seed.js
@@ -74,6 +74,8 @@ export const photoGallery = [
     
 ]
 
+
+
 export const NavbarData = [
 
     {
