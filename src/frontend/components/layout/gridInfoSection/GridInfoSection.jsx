4feccafbd49b4eaf4d75e61905d53a0e1bd2697e
Trinidad Padilla <tpadilla101@live.com>
2022-05-19 16:43:56 -0700

    working on the about page

---
+++ src/frontend/components/layout/gridInfoSection/GridInfoSection.jsx
@@ -0,0 +1,120 @@
+import React, {useMemo} from 'react';
+import { useInView } from 'react-intersection-observer';
+
+function GridInfoSection( {
+    ptext, 
+    ptext2, 
+    headingtxt, 
+    gridInfoSectionClassName, 
+    gridInfoSectionTxtParent,
+    gridInfoSectionTxtWrapper1ClassName, 
+    gridInfoSectionTxtWrapper2ClassName, 
+    gridInfoSectionHeadingContainer,
+    gridInfoSectionImg, 
+    gridInfoSectionImg2, 
+    alt, 
+    content } ) {
+
+
+  const insersectionOptions = useMemo( ()=> {
+    return {
+      threshold: 1,
+      root: null, 
+      rootMargin: '0px 0px -100px 0px',
+      triggerOnce: true
+    }
+   }, []);
+    
+    const { ref: paragraph1Ref, inView: isVisible, }= useInView(insersectionOptions);
+
+  const insersectionOptions2 = useMemo( ()=> {
+    return {
+      threshold: 1,
+      root: null, 
+      rootMargin: '0px 0px -100px 0px',
+      triggerOnce: true
+    }
+   }, []);
+    
+    const { ref: paragraph2Ref, inView: isVisible2, }= useInView(insersectionOptions2);
+
+  const headingIntersectionOptions = useMemo( ()=> {
+    return {
+      threshold: 1,
+      root: null, 
+      rootMargin: '0px 0px -100px 0px',
+      triggerOnce: true
+    }
+   }, []);
+    
+    const { ref: headingRef, inView: headingIsVisible, }= useInView(headingIntersectionOptions);
+
+  const imgInteresctionOptions = useMemo( ()=> {
+    return {
+      threshold: 1,
+      root: null, 
+      rootMargin: '0px 0px -350px 0px',
+      triggerOnce: true
+    }
+   }, []);
+
+  const { ref: imgRef, inView: imgIsVisible, }= useInView(imgInteresctionOptions);
+
+
+  return (
+    <section className={gridInfoSectionClassName}>
+        <div className={gridInfoSectionTxtParent}>
+            <div className={gridInfoSectionTxtWrapper1ClassName}>
+            
+        {/* Side Img 1: */}
+        
+            <div className={imgIsVisible ? 'gridInfoSection-img-parent slide-in' : 'gridInfoSection-img-parent'} /* ref={imgRef} */>
+            
+                <img src={gridInfoSectionImg} alt={alt} className='gridInfoSection-img' />
+
+            
+
+            </div>
+
+            <div className={gridInfoSectionHeadingContainer}>
+                <h2 ref={paragraph1Ref} className={isVisible ? 'gridInfoSection-p1 fadeInUp active' : 'gridInfoSection-p1'} >
+                    { isVisible ? ptext : ''}
+                </h2>
+            </div>
+
+            
+            
+            </div>
+
+            <div className={gridInfoSectionTxtWrapper2ClassName}>
+            
+
+        {/* Side Img 2: */}
+        
+            <div className={gridInfoSectionHeadingContainer}>
+                <h2 ref={paragraph2Ref} className={isVisible2 ? 'gridInfoSection-p2 fadeInUp active' : 'gridInfoSection-p2'}>
+                    { isVisible2 ? ptext2 : ''}
+                </h2>
+            </div>
+
+            <div className={imgIsVisible ? 'gridInfoSection-img-parent2 slide-in' : 'gridInfoSection-img-parent2'} ref={imgRef}>
+            
+                <img src={gridInfoSectionImg2} alt={alt} className='gridInfoSection-img' />
+            
+            
+
+            </div>
+
+
+                
+            
+            </div>
+
+        </div>
+
+
+    </section>
+  )
+}
+
+export default GridInfoSection;
--- src/frontend/components/pages/about/About.js
+++ src/frontend/components/pages/about/About.js
@@ -2,16 +2,19 @@ import React from 'react';
 import {
   Footer,
   HeroCarousel,
-  InfoSection
+  InfoSection,
+  GridInfoSection
 } from '../../utils.js';
 
 import { heroCarouselData } from '../../../seed';
 import AmandaSmiling from '../../../assets/images/Amanda-04.jpeg';
+import Amanda2 from '../../../assets/images/Amanda-46.jpeg';
+import Amanda3 from '../../../assets/images/Amanda-62.jpeg';
 
 function About() {
   return (
     <div className='aboutpg-parent-container'>
-        {/* Greetings from About */}
+        
         <HeroCarousel imgData={heroCarouselData} />
         <InfoSection 
           ptext={'"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.'}
@@ -23,6 +26,16 @@ function About() {
           alt={'Somne sample text'}
           infoSectionTxtWrapperClassName={'aboutpg-infoSection-txt-parent-container'}
         
+        />
+        <GridInfoSection
+          gridInfoSectionClassName={'aboutpg-gridInfoSection-parent-container'}
+          gridInfoSectionTxtParent={'aboutpg-gridInfoSection-txt-parent-container'}
+          gridInfoSectionTxtWrapper1ClassName={'aboutpg-gridInfoSection-txt-wrapper1'}
+          gridInfoSectionTxtWrapper2ClassName={'aboutpg-gridInfoSection-txt-wrapper2'}
+          ptext={'Turtles has a lot to offer to her students whether they are novice or seasoned.'}
+          ptext2={'With over 5 years in industry, Turtles has gained a wealth of experience in many areas over a long time.'}
+          gridInfoSectionImg={Amanda2}
+          gridInfoSectionImg2={Amanda3}
         />
         <Footer />
     </div>
