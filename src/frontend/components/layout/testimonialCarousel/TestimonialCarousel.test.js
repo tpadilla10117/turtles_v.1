291801641d53e5a476f32bcd6708258f184b9a13
Trinidad Padilla <tpadilla101@live.com>
2022-06-28 18:10:04 -0700

    working on last test

--- src/frontend/components/layout/testimonialCarousel/TestimonialCarousel.test.js
+++ src/frontend/components/layout/testimonialCarousel/TestimonialCarousel.test.js
@@ -1,6 +1,7 @@
 import { shallow, mount } from 'enzyme';
 import React from 'react';
 import TestimonialCarousel from './TestimonialCarousel';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 describe('The TestimonialCarousel.jsx component:', () => {
 
@@ -18,25 +19,53 @@ describe('The TestimonialCarousel.jsx component:', () => {
         }
     ]
 
-    it('renders the comoponent', () => {
+    it('renders the component', () => {
         expect.assertions(2);
         expect(shallow(<TestimonialCarousel carouselData={mockPhotoProps}/>).length).toEqual(1);
         expect(shallow(<TestimonialCarousel carouselData={mockPhotoProps} />)).toMatchSnapshot();
-    })
+    });
 
     it('renders one article tag', () => {
         const wrapper = shallow(<TestimonialCarousel carouselData={mockPhotoProps} />);
+        expect.assertions(2);
         expect(wrapper.find('article').length).toEqual(1);
-    })
+        expect(wrapper).not.toBeNull();
+    });
 
-    it('should update state on click', () => {
-        const nextSlide = jest.fn();
+    it('renders 2 SVG icons', () => {
+        const wrapper = shallow(<TestimonialCarousel carouselData={mockPhotoProps} />);
+
+        expect.assertions(2);
+        expect(wrapper.find('.testimonialCarousel-forwardarrow').length).toEqual(1);
+        expect(wrapper.find('.testimonialCarousel-backarrow').length).toEqual(1);
+    });
+
+    it('should ensure a function is passed to material UI icon onClick props', () => {
+    /* Define a mock function: */
+        const mockComponentWrapper = shallow(<TestimonialCarousel carouselData={mockPhotoProps} />);
+
+    /* Define a mock function to validate whether calling it actually valls the useState hook: */
+        expect.assertions(5);
+        expect(mockComponentWrapper.length).toEqual(1);
+        
+        const container = mockComponentWrapper.find('.testimonialCarousel-forwardarrow');
+        expect(container.length).toEqual(1)
+        expect(container.prop('onClick')).toBeTruthy();
+
+        const container2 = mockComponentWrapper.find('.testimonialCarousel-backarrow');
+        expect(container2.length).toEqual(1)
+        expect(container2.prop('onClick')).toBeTruthy();
+    });
+
+    it('checks the current slide', () => {
         const mockComponentWrapper = mount(<TestimonialCarousel carouselData={mockPhotoProps} />);
+        expect(mockComponentWrapper.length).toEqual(1);
 
-        const handleClick = jest.spyOn(React, "useState");
-        handleClick.mockImplementation(slide => [slide, nextSlide]);
+        const container = mockComponentWrapper.find('.testimonialCarousel-img-slider');
+        expect(container.length).toEqual(1);
 
-        mockComponentWrapper.find("#testimonialCarousel-forwardarrow").simulate("click");
-        expect(nextSlide).toBeCalled();
+        const container2 = container.find('.testimonialCarousel-slide active');
+        console.log(container2.debug())
+        /* expect(container2.length).toEqual(1);   */      
     })
 })
